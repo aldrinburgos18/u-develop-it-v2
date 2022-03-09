65da6245271369d53075751966fb6d4cec67c3ea
aldrinburgos <burgos.aldrin@gmail.com>
2022-03-09 16:17:24 -0600

    set up SQL database

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const sqlite3 = require("sqlite3").verbose();
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -6,11 +8,25 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = new sqlite3.Database("./db/election.db", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the election database");
+});
+
+db.all(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 //catch all response(not found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+//start server after DB connection
+db.on("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
