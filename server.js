a7cd34e5c6f91ab20717d438a82d1486e2cab985
aldrinburgos <burgos.aldrin@gmail.com>
2022-03-09 19:40:14 -0600

    add post route

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const inputCheck = require("./utils/inputCheck");
+
 const db = new sqlite3.Database("./db/election.db", (err) => {
   if (err) {
     return console.error(err.message);
@@ -68,15 +70,33 @@ app.delete("/api/candidates/:id", (req, res) => {
 });
 
 //Create a candidate
-/* const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-             VALUES (?,?,?,?)`;
-const params = [1, "Ronald", "Firbank", 1];
-db.run(sql, params, function (err, result) {
-  if (err) {
-    console.log(err);
+app.post("/api/candidates", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
   }
-  console.log(result, this.lastID);
-}); */
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+      id: this.lastID,
+    });
+  });
+});
 
 //catch all response(not found)
 app.use((req, res) => {
