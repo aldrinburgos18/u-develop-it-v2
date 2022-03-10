e9e6940e4f7efacf6a369ec08ef97b4986f1ce1a
aldrinburgos <burgos.aldrin@gmail.com>
2022-03-09 18:21:06 -0600

    get, delete and create a candidate

--- server.js
+++ server.js
@@ -19,6 +19,33 @@ db.all(`SELECT * FROM candidates`, (err, rows) => {
   console.log(rows);
 });
 
+//GET a single candidate
+db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(row);
+});
+
+//DELETE a candidate
+db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result, this, this.changes);
+});
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+             VALUES (?,?,?,?)`;
+const params = [1, "Ronald", "Firbank", 1];
+db.run(sql, params, function (err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result, this.lastID);
+});
+
 //catch all response(not found)
 app.use((req, res) => {
   res.status(404).end();
