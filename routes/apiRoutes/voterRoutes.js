832393e01f5f498fcb7424579bd7078a67f839c6
aldrinburgos <burgos.aldrin@gmail.com>
2022-03-10 21:53:36 -0600

    add voter create route

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -3,6 +3,7 @@ const db = require("../../db/database");
 
 const inputCheck = require("../../utils/inputCheck");
 
+//get all voters
 router.get("/voters", (req, res) => {
   const sql = `SELECT * FROM voters ORDER BY last_name`;
   const params = [];
@@ -19,6 +20,7 @@ router.get("/voters", (req, res) => {
   });
 });
 
+//get a single voter
 router.get("/voters/:id", (req, res) => {
   const sql = `SELECT * FROM voters WHERE id = ?`;
   const params = [req.params.id];
@@ -31,6 +33,31 @@ router.get("/voters/:id", (req, res) => {
     res.json({
       message: "success",
       data: rows,
+    });
+  });
+});
+
+//add a voter
+router.post("/voters/", ({ body }, res) => {
+  const errors = inputCheck(body, "first_name", "last_name", "email");
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO voters (first_name, last_name, email)
+                 VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.email];
+
+  db.run(sql, params, function (err, data) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "Voter successfully created",
+      data: body,
+      id: this.lastID,
     });
   });
 });
